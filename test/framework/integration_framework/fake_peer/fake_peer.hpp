3178d79c32867e3fc7b7e1a5a015938a7042f1bb
Mikhail Boldyrev <miboldyrev@gmail.com>
2018-11-14 16:19:21 +0300

    struct forward-declaration
    Signed-off-by: Mikhail Boldyrev <miboldyrev@gmail.com>

--- test/framework/integration_framework/fake_peer/fake_peer.hpp
+++ test/framework/integration_framework/fake_peer/fake_peer.hpp
@@ -41,9 +41,9 @@ namespace iroha {
   namespace consensus {
     namespace yac {
       class NetworkImpl;
-      class VoteMessage;
       class YacCryptoProvider;
       class YacHash;
+      struct VoteMessage;
     }  // namespace yac
   }    // namespace consensus
   namespace ordering {
