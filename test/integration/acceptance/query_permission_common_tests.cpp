632df1c6bb8ec5d77849fb0173f5b0f80c640758
Konstantin Munichev <toobwn@gmail.com>
2019-01-18 14:00:38 +0300

    Improve TODOs for QueryPermission acceptance test
    Signed-off-by: Konstantin Munichev <toobwn@gmail.com>

--- test/integration/acceptance/query_permission_common_tests.cpp
+++ test/integration/acceptance/query_permission_common_tests.cpp
@@ -20,7 +20,7 @@ using QueryPermissionTestingTypes =
 TYPED_TEST_CASE(QueryPermissionFixture, QueryPermissionTestingTypes);
 
 /**
- * TODO mboldyrev 05.12.2018 IR-56 remove? covered by field validator test
+ * TODO mboldyrev 18.01.2019 IR-219 remove, covered by field validator test
  *
  * Get data from a non-existing account
  * @given a user with permission to read all accounts
@@ -41,7 +41,7 @@ TYPED_TEST(QueryPermissionFixture,
 }
 
 /**
- * TODO mboldyrev 05.12.2018 IR-56 remove? covered by field validator test
+ * TODO mboldyrev 18.01.2019 IR-219 remove, covered by field validator test
  *
  * Pass an empty account id
  * @given a user with permission to read all accounts
@@ -60,7 +60,7 @@ TYPED_TEST(QueryPermissionFixture, ReadEmptyAccountHavingPermissionForAll) {
 }
 
 /*
- * TODO mboldyrev 05.12.2018 IR-56 convert to a SFV integration test
+ * TODO mboldyrev 18.01.2019 IR-219 convert to a SFV integration test
  *
  * Below are test cases that check different combinations of permission modes
  * (1 - no permissions,   2 - can_get_my_*,
