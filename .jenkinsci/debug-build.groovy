d9987052aaaf7a21c9395c5814a1f229295ffbcd
Alexey Rodionov <rodionov@soramitsu.co.jp>
2019-01-26 19:01:27 +0300

    new txt input
    Signed-off-by: Alexey Rodionov <rodionov@soramitsu.co.jp>

--- .jenkinsci/debug-build.groovy
+++ .jenkinsci/debug-build.groovy
@@ -23,6 +23,7 @@ def doDebugBuild(coverageEnabled=false) {
   for(String el : files.split("\\r?\\n")) {
     plots << [displayTableFlag: true, exclusionValues: '', file: el, inclusionFlag: 'OFF', url: '']
   }
+  println plots
   plot csvFileName: 'plot-3d136de2-a268-4abc-80a1-9f31db39b92d.csv', 
     csvSeries: [plots], 
     group: 'iroha_build_time_graph', 
