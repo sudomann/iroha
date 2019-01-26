5e0463a421b43edd6daf33120109173efc2ba5eb
Alexey Rodionov <rodionov@soramitsu.co.jp>
2019-01-26 19:14:25 +0300

    new txt input
    Signed-off-by: Alexey Rodionov <rodionov@soramitsu.co.jp>

--- .jenkinsci/debug-build.groovy
+++ .jenkinsci/debug-build.groovy
@@ -21,9 +21,7 @@ def doDebugBuild(coverageEnabled=false) {
   def plots = []
   def files = sh(script: 'ls reports/*.csv', returnStdout: true).trim() //findFiles(glob: 'reports/*.csv')
   for(String el : files.split("\\r?\\n")) {
-    plots << [displayTableFlag: true, exclusionValues: '', file: el, inclusionFlag: 'OFF']
-  }
-  println plots
+    // plots << [displayTableFїlag: true, exclusionValues: '', file: el, inclusionFlag: 'OFF']
   plot csvFileName: 'plot-3d136de2-a268-4abc-80a1-9f31db39b92d.csv', 
     csvSeries: plots, 
     group: 'iroha_build_time_graph', 
@@ -34,9 +32,25 @@ def doDebugBuild(coverageEnabled=false) {
     yaxisMinimum: '0',
     yaxisMaximum: '10',
     hasLegend: false,
-    title: 'Build time',
+    title: (Math.abs( new Random().nextInt() % (99 - 40) ) + 40).toString(), 
     useDescr: false,
     yaxis: 'Time, sec'
 }
+  }
+//   println plots
+//   plot csvFileName: 'plot-3d136de2-a268-4abc-80a1-9f31db39b92d.csv', 
+//     csvSeries: plots, 
+//     group: 'iroha_build_time_graph', 
+//     numBuilds: '1', 
+//     style: 'line', 
+//     width: 4000,
+//     height: 3000,
+//     yaxisMinimum: '0',
+//     yaxisMaximum: '10',
+//     hasLegend: false,
+//     title: 'Build time',
+//     useDescr: false,
+//     yaxis: 'Time, sec'
+// }
 
 return this
