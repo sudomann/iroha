97058e30df030ce37248cbab9e4296e99b83c5f2
Alexey Rodionov <rodionov@soramitsu.co.jp>
2019-01-29 15:46:07 +0300

    New input
    Signed-off-by: Alexey Rodionov <rodionov@soramitsu.co.jp>

--- .jenkinsci/debug-build.groovy
+++ .jenkinsci/debug-build.groovy
@@ -16,26 +16,26 @@ def doDebugBuild(coverageEnabled=false) {
 
 
   sh("python analyze.py result.txt")
-
-
-  def plots = []
-  def files = sh(script: 'ls reports/*.csv', returnStdout: true).trim() //findFiles(glob: 'reports/*.csv')
-  for(String el : files.split("\\r?\\n")) {
-    // plots << [displayTableFїlag: true, exclusionValues: '', file: el, inclusionFlag: 'OFF']
-  plot csvFileName: 'plot-3d136de2-a268-4abc-80a1-9f31db39b92d.csv', 
-    csvSeries: plots, 
-    group: 'iroha_build_time_graph', 
-    numBuilds: '1', 
-    style: 'line', 
-    width: 4000,
-    height: 3000,
-    yaxisMinimum: '0',
-    yaxisMaximum: '10',
-    hasLegend: false,
-    title: (Math.abs( new Random().nextInt() % (99 - 40) ) + 40).toString(), 
-    useDescr: false,
-    yaxis: 'Time, sec'
-}
+  zip archive: true, dir: '', glob: 'result.txt', zipFile: 'results.zip'
+  archiveArtifacts artifacts: '*.zip'
+//   def plots = []
+//   def files = sh(script: 'ls reports/*.csv', returnStdout: true).trim() //findFiles(glob: 'reports/*.csv')
+//   for(String el : files.split("\\r?\\n")) {
+//     // plots << [displayTableFїlag: true, exclusionValues: '', file: el, inclusionFlag: 'OFF']
+//   plot csvFileName: 'plot-3d136de2-a268-4abc-80a1-9f31db39b92d.csv', 
+//     csvSeries: plots, 
+//     group: 'iroha_build_time_graph', 
+//     numBuilds: '1', 
+//     style: 'line', 
+//     width: 4000,
+//     height: 3000,
+//     yaxisMinimum: '0',
+//     yaxisMaximum: '10',
+//     hasLegend: false,
+//     title: (Math.abs( new Random().nextInt() % (99 - 40) ) + 40).toString(), 
+//     useDescr: false,
+//     yaxis: 'Time, sec'
+// }
   }
 //   println plots
 //   plot csvFileName: 'plot-3d136de2-a268-4abc-80a1-9f31db39b92d.csv', 
